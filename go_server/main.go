310aee545dbfb72993330406a0f6ca2f8330c3db
TaricOv <tareq.ouf@gmail.com>
2023-04-04 22:09:28 +0200

    connected db

--- go_server/main.go
+++ go_server/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+"go_server/database"
 "go_server/routes"
 "github.com/gofiber/fiber/v2"
 )
 
 func main() {
+database.Connect()
     app := fiber.New()
 routes.Setup(app)
    app.Listen("localhost:8080")
