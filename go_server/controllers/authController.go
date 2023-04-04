d444e1086bf7029e566311b7c3a9b26deb807574
TaricOv <tareq.ouf@gmail.com>
2023-04-04 08:51:34 +0200

    register route was added

--- go_server/controllers/authController.go
+++ go_server/controllers/authController.go
@@ -3,8 +3,14 @@ package controllers
 import "github.com/gofiber/fiber/v2"
 
 
-    func Hello(c *fiber.Ctx) error {
-        return c.SendString("Hello, World 👋!")
+    func Register(c *fiber.Ctx) error {
 
+var data map[string]string
+
+err := c.BodyParser(&data)
+if err != nil{
+return err
+}
+return c.JSON(data)
     }
 
